06e2591cd4719909873fe55521639d4c5275e211
Anton Fedosov <fedosov-toxa@mail.ru>
2023-11-09 11:29:26 +0300

    add time(str) field in DB

--- client/main.py
+++ client/main.py
@@ -60,6 +60,22 @@ class MyThreadTest2(QtCore.QThread):
             print(self.count_winners)
 
 
+class MyThreadVariant(QtCore.QThread):
+    mysignal = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        QtCore.QThread.__init__(self, parent)
+        self.time_format = None
+        self.curr_answers = None
+
+    def run(self):
+        for i in range(2400, -1, -1):
+            self.sleep(1)
+            self.time_format = time.strftime("%H:%M:%S", time.gmtime(i))
+            result_time[0] = str(i)
+            self.mysignal.emit("Осталось времени = %s" % self.time_format)
+
+
 class Client:
     def __init__(self, ip, port):
         self.cli = socket(AF_INET, SOCK_STREAM)
