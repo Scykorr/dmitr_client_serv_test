fc2218d6f9b61a62b0e5e5ec09924f4e2912db02
Anton Fedosov <fedosov-toxa@mail.ru>
2023-11-20 14:55:11 +0300

    add task6var1part2 for check

--- server/db_view/main_variant.py
+++ server/db_view/main_variant.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import QTableWidgetItem, QApplication
 from server.db_view.GUIpy.main_variant import Ui_Form
 import sqlite3 as sql
 from variant_1 import (Task1Part1Var1, Task2Part1Var1, Task345Part1Var1, Task6Part1Var1, Task7Part1Var1,
-                       Task8Part1Var1, Task123Part2Var1, Task4Part2Var1, Task5Part2Var1)
+                       Task8Part1Var1, Task123Part2Var1, Task4Part2Var1, Task5Part2Var1, Task6Part2Var1)
 
 
 class WindowVariantMain(QtWidgets.QWidget):
@@ -29,6 +29,7 @@ class WindowVariantMain(QtWidgets.QWidget):
         self.task123var1part2 = Task123Part2Var1()
         self.task4var1part2 = Task4Part2Var1()
         self.task5var1part2 = Task5Part2Var1()
+        self.task6var1part2 = Task6Part2Var1()
 
     def drop_db(self):
         con = sql.connect('../data.db')
@@ -110,6 +111,10 @@ class WindowVariantMain(QtWidgets.QWidget):
             self.username = self.ui_main_server.tableWidget_server.item(curr_row, 0).text()
             self.task5var1part2.main_select(self.username)
             self.task5var1part2.show()
+        elif curr_column == 14 and self.ui_main_server.tableWidget_server.item(curr_row, 1).text() == '1':
+            self.username = self.ui_main_server.tableWidget_server.item(curr_row, 0).text()
+            self.task6var1part2.main_select(self.username)
+            self.task6var1part2.show()
 
 
 if __name__ == '__main__':
